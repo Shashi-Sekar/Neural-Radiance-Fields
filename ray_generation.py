aa3cf36391b47885051d0cea887cd4e5bed510aa
Shashi-Sekar <shashi.dhanasekar@gmail.com>
2024-09-03 16:54:23 -0700

    Added Hierarchical Sampling

--- ray_generation.py
+++ ray_generation.py
@@ -41,20 +41,70 @@ def ray_generation(H: int, W: int, focal_length: float, c2w: torch.Tensor) -> to
 
     return ray_origins, ray_directions_world
 
-def hierarchical_sampling(bins: int, weights: torch.tensor, num_samples: int, uniform=False):
+def hierarchical_sampling(bins: torch.tensor, weights: torch.tensor, num_samples: int, uniform=False):
     '''
     Hierarchical sampling -> to sample more points from those positions we expect to have visual content
     Based on the evaluation of the 'coarse' network on stratified samples
     Parameters:
-        bins:           Number of bins used for stratified sampling
-        weights:        Weights for the bins
+        bins:           Bins for stratified sampling, shape: (num_rays, num_samples)
+        weights:        Weights for the bins, shape: (num_rays, num_samples)
         num_samples:    Number of samples
-        uniform:        Whether to perform uniform sampling
+        uniform:        Whether the samples are equally spaced
     '''
+    num_rays, _ = weights.shape
 
-    pdf = weights / torch.sum(weights, dim=-1)
+    #Overcome NaN values
+    weights = weights + 1e-6
 
-    pass
+    #Normalize the weights to get the PDF, shape: (num_rays, num_samples)
+    pdf = weights / torch.sum(weights, dim=-1, keepdim=True)
+
+    #Compute the CDF, shape: (num_rays, num_samples)
+    cdf = torch.cumsum(pdf, dim=-1)
+    
+    #Add zero as the first entry -> CDF is 0 for values < a if the domain is [a, b], shape: (num_rays, num_samples+1)
+    cdf = torch.cat([torch.zeros_like(cdf[..., :1]), cdf], dim=-1)
+
+    #Generate random samples from a uniform distribution (0, 1)
+    if uniform:
+        #Uniform - Equally spaced samples
+        u = torch.linspace(0, 1, num_samples)
+        #Matching the shape of CDF expect for the last dimension which should now be the number of samples, shape: (num_rays, num_samples)
+        #u = u.expand(list(cdf.shape[:-1]) + [num_samples])
+        u = u.expand(num_rays, num_samples)
+    else:
+        #Random, shape: (num_rays, num_samples)
+        #u = torch.rand(list(cdf.shape[:-1]) + [num_samples])
+        u = torch.rand(num_rays, num_samples)
+    
+    #Make the tensor contiguous to avoid memory issues
+    u.contiguous()
+
+    #Inverting the CDF
+    #Find the first bin where the CDF is greater than the corresponding random sample
+    indices = torch.searchsorted(cdf, u, right=True)
+
+    #Avoid out of bounds
+    lower_bound = torch.max(torch.zeros_like(indices-1), indices-1)
+    higher_bound = torch.min(torch.ones_like(indices)*num_samples, indices)
+    indices_bound = torch.stack([lower_bound, higher_bound], dim = -1)
+    
+    #Matching the the number dimensions of CDF and indices
+    #For each ray, each sample, we need the lower and higher bounds
+    matched_shape = [indices_bound.shape[0], indices_bound.shape[1], cdf.shape[-1]]
+    cdf_bound = torch.gather(cdf.unsqueeze(1).expand(matched_shape), 2, indices_bound)
+    bins_bound = torch.gather(bins.unsqueeze(1).expand(matched_shape), 2, indices_bound)
+
+    #Linear Interpolation
+    # if u is between lower_cdf and higher_cdf: x = x0 + (x1 - x0) * (u - lower_cdf) / (upper_cdf - lower_cdf)
+    # if cdf exists, then u - lower_cdf = 0, so x = x0
+    denom = (cdf_bound[..., 1] - cdf_bound[..., 0])
+    denom = torch.where(denom < 1e-5, torch.ones_like(denom), denom)
+    t = (u - cdf_bound[..., 0]) / denom
+    samples = bins_bound[..., 0] + t * (bins_bound[..., 1] - bins_bound[..., 0])
+
+    return samples
+
 def rays_ndc(H, W, focal, near, rays_origin, rays_direction):
     '''
     Generate the rays in NDC space (Normalized Device Coordinates)
@@ -74,18 +124,27 @@ def rays_ndc(H, W, focal, near, rays_origin, rays_direction):
     pass
 
 if __name__ == "__main__":
+    '''
     H = 4
     W = 4
     focal_length = 1.0
     c2w = torch.ones(4, 4)
 
-    '''
     c2w = torch.tensor([[1/np.sqrt(2), 0, 1/np.sqrt(2), 1], 
                         [0, 1, 0, 1],
                         [-1/np.sqrt(2), 0, 1/np.sqrt(2), 1],
                         [0, 0, 0, 1]])
-    '''
-
+    
     ray_origins, ray_directions = ray_generation(H, W, focal_length, c2w, False)
     print(ray_origins.shape, ray_directions.shape)
     print(ray_origins[0, 0], ray_directions[0, 0])
+    
+    '''
+    
+    pdf = torch.rand(4,3)
+    bins = torch.tensor([1.0, 2.0, 3.0])
+    num_samples = 3
+
+    samples = hierarchical_sampling(bins, pdf, num_samples, False)
+
+    print(samples)
